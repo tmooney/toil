cb758ece159e6f2c5883a30d429993cc1a8464f0
ejacox <jacox_edwin@hotmail.com>
2017-11-03 12:38:09 -0700

    added gsutil

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def runSetup():
                 'pynacl==1.1.2'],
             'google': [
                 'gcs_oauth2_boto_plugin==1.9',
-                'gsutils',
+                'gsutil',
                 botoRequirement],
             'cwl': [
                 'cwltool==1.0.20170822192924',
